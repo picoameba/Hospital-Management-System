7c3f1d59ecdac049c2dc910837512d460d88813b
Jeffer-Roshak <57754929+Jeffer-Roshak@users.noreply.github.com>
2019-11-15 20:22:19 +0400

    Update Person.java
    I thought we could use the REGEX package but it has some limitations. We can use it to filter some basic errors but nothing else. I think its better to use other string methods to cut up the string and then do checks.

--- Hospital-Management-System2/src/project/Person.java
+++ Hospital-Management-System2/src/project/Person.java
@@ -1,5 +1,23 @@
 package project;
+import java.util.regex.*;//Regular expression package, helps us easily check formats and patterns in Strings
 
 public class Person {
-
+private String Name, Nationality, Date_of_Birth, Mobile_Number, Email;
+private int age;
+public bool Date_of_Birth_Checker(String DoB){
+  //To verify if the given String has the correct date format; returns true or false
+  if(DoB.matches("Insert basic pattern here))
+  {   
+  }
+  else
+    return false;
+}
+public bool Mobile_Number_Checker(String number){
+  //to verify if the given String has the correct phone number format; returns true or false
+  return number.matches();
+}
+public bool Email_Checker(String mail){
+  //to verify if the given String has the correct mail format; returns true or false
+  return mail.matches();
+}
 }
